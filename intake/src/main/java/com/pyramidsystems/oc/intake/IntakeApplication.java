c61c9ebe5de0194daf5aaf3ae42d4a976444b552
swatturkar <swatturkar@g.harvard.edu>
2018-11-20 18:08:48 -0500

    Added initial version of the application

--- intake/src/main/java/com/pyramidsystems/oc/intake/IntakeApplication.java
+++ intake/src/main/java/com/pyramidsystems/oc/intake/IntakeApplication.java
@@ -2,11 +2,21 @@ package com.pyramidsystems.oc.intake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IntakeApplication {
+public class IntakeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntakeApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<IntakeApplication> applicationClass = IntakeApplication.class;
+
 }
